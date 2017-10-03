9d7cd47db074a5145739dca38dd6a516e86ac7b3
Jacob Meixner <jjm3333@gmail.com>
2017-10-02 20:39:35 -0700

    Convert to java project using Inellij
    One thing to note is the .iml file here is looking for the core.jar
    for processing in the Downloads directory of the user which should
    be updated to be more reasonable

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
+    }
+
+    public void settings() {
+        size(300,300);
+    }
+
+    public void setup() {
+        fill(120,50,240);
+    }
+
+    public void draw() {
+        ellipse(width/2,height/2,second(), second());
+    }
+
+}
+
