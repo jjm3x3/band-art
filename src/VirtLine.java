9b2037a25ecba86ff3a3cb14ab63fd841f27f95b
Jacob Meixner <jjm3333@gmail.com>
2017-10-15 12:09:42 -0700

    Clean up some comments and spelling

--- src/VirtLine.java
+++ src/VirtLine.java
@@ -27,10 +27,14 @@ class VirtLine {
         y2 = 360;
     }
 
+    // debug will print the four float values which
+    // represent the two points of this object
     void debug() {
         parent.println("x1: " + x1 + ", y1: " + y1 + ", x2: ", + x2 + ", y2: " + y2);
     }
 
+    // pointDebug prints out the four float values
+    // in a pretty point looking format
     void pointDebug() {
         parent.println("p1: (" + x1 + ", " + y1 + ") p2: (", + x2 + ", " + y2 +")");
     }
@@ -39,14 +43,15 @@ class VirtLine {
         // could/ should return a normalized vector
     }
 
-    // will move only orthogonaly to itself
+    // will move only orthogonally to itself
+    // if left is true it will move leftward
     void moveOrth() {
         float v1 = x2 - x1;
         float v2 = y2 - y1;
-        // solve for an orthoganal vector (a,b)
-        float b = -v1 / v2;  //asumes "a" is 1
+        // solve for an orthogonal vector (a,b)
+        float b = -v1 / v2;  //assumes "a" is 1
 
-        // unitize the orthoganal vector
+        // unitize the orthogonal vector
         float orthLen = parent.sqrt(parent.pow(1,2) + parent.pow(b,2));
         float orthU1 = 1/ orthLen;
         float orthU2 = b/ orthLen;
@@ -58,9 +63,9 @@ class VirtLine {
         y2 = y2 + orthU2;
     }
 
-    // Will move in a standard unifom direction
+    // Will move in a standard uniform direction
     void move() {
-        // asume every line is going down and to the right at 45 degress
+        // assume every line is going down and to the right at 45 degrees
         x1 += 0.85;
         y1 += 0.85;
         x2 += 0.85;
