bf6969ee1132591b38a0ecd1bee1511b90e340d2
Jacob Meixner <jjm3333@gmail.com>
2017-10-15 21:53:46 -0700

    Replace need to draw them all in the main thread

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,8 @@ public class Main extends PApplet {
         scene1 = new VirtLineScene(this);
 
         seq1 = new DotSequence(this, 20, 15);
+        seq2 = new DotSequence(this, 15, 25);
+        seq3 = new DotSequence(this, 20, 35);
     }
 
     public void draw() {
@@ -34,24 +36,28 @@ public class Main extends PApplet {
 
     float movingX = 15;
     DotSequence seq1;
+    DotSequence seq2;
+    DotSequence seq3;
     void dotScene() {
         background(0);
 
         strokeWeight(10);
-        stroke(255);
-        for (int i = 0; i < 20; ++i) {
-            stroke(255, 0 + i * 12);
-            point(movingX + i * 10, 25);
-            point(movingX + 5 + i * 10, 35);
-        }
-
-        movingX += 5;
-
-        if (movingX >= 640) {
-            movingX = 0;
-        }
+//        stroke(255);
+//        for (int i = 0; i < 20; ++i) {
+//            stroke(255, 0 + i * 12);
+//            point(movingX + i * 10, 25);
+//            point(movingX + 5 + i * 10, 35);
+//        }
+//
+//        movingX += 5;
+//
+//        if (movingX >= 640) {
+//            movingX = 0;
+//        }
 
         seq1.draw();
+        seq2.draw();
+        seq3.draw();
         try {
             Thread.sleep(50);
         } catch (InterruptedException e) {
